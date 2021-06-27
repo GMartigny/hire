e0d5c2f86c9c84380813f487c2d9f94eadf23d0f
Guillaume Martigny <guillaume.martigny@gmail.com>
2021-06-27 14:18:33 +0200

    Refacto

--- api/index.js
+++ api/index.js
@@ -3,10 +3,8 @@ const allow = require("allow-cors");
 module.exports = (request, response) => {
     allow(response);
 
-    const payload = {
+    response.json({
         available: false,
         email: "guillaume.martigny@gmail.com",
-    };
-
-    response.json(payload);
+    });
 };
